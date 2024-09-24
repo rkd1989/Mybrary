e64688916a31d2b994b8d1c355556315528fcfca
Rishabh Kavidayal <34520858+rkd1989@users.noreply.github.com>
2024-09-24 12:09:27 -0500

    Book create index new

---
+++ routes/books.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const { route } = require(".");
+const Book = require("../model/book");
+const router = express.Router();
+const Author = require("../model/author");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const uploadPath = path.join("public", Book.coverImageBasePath);
+const imageMimeTypes = ["image/jpeg", "image/png", "images/gif", "image/jpg"];
+const upload = multer({
+  dest: uploadPath,
+  fileFilter: (req, file, callback) => {
+    callback(null, imageMimeTypes.includes(file.mimetype));
+  },
+});
+//All Book Routes
+router.get("/", async (req, res) => {
+  let query = Book.find();
+  if (req.query.title != null && req.query.title !== "") {
+    query = query.regex("title", new RegExp(req.query.title, "i"));
+  }
+  if (req.query.publishedBefore != null && req.query.publishedBefore !== "") {
+    query = query.lte("publishDate", req.query.publishedBefore);
+  }
+  if (req.query.publishedAfter != null && req.query.publishedAfter !== "") {
+    query = query.gte("publishDate", req.query.publishedAfter);
+  }
+  try {
+    const books = await query.exec();
+    res.render("books/index", { books: books, searchOptions: req.query });
+  } catch (error) {
+    res.redirect("/");
+  }
+});
+
+//New Book Route
+router.get("/new", async (req, res) => {
+  renderNewPage(res, new Book());
+});
+
+// Create Book Route
+router.post("/", upload.single("cover"), async (req, res) => {
+  console.log(req.file);
+  const filename = req.file != null ? req.file.filename : null;
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    description: req.body.description,
+    publishDate: new Date(req.body.publishDate),
+    pageCount: req.body.pageCount,
+    coverImageName: filename,
+    description: req.body.description,
+  });
+  try {
+    const newBook = await book.save();
+    //res.redirect(`books/${newBook.id}`);
+    res.redirect(`books`);
+  } catch (error) {
+    //console.log(error);
+    if (book.coverImageName != null) {
+      removeBookCover(book.coverImageName);
+    }
+    renderNewPage(res, book, true);
+  }
+});
+
+function removeBookCover(fileName) {
+  fs.unlink(path.join(uploadPath, fileName), (err) => {
+    if (err) console.error(err);
+  });
+}
+
+async function renderNewPage(res, book, hasError = false) {
+  try {
+    const authors = await Author.find({});
+    const params = {
+      authors: authors,
+      book: book,
+    };
+    if (hasError) params.errorMessage = "Error Creating Book";
+    res.render("books/new", params);
+  } catch (error) {
+    res.redirect("/books");
+  }
+}
+
+module.exports = router;
